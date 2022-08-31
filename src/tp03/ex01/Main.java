20f0fdeaeb958211ef845a18178fcb8dde219783
Sebastian Iovaldi <sebastian.iovaldi@est.fi.uncoma.edu.ar>
2022-08-31 18:02:37 -0300

    tp3 ex 1

---
+++ src/tp03/ex01/Main.java
@@ -0,0 +1,13 @@
+package tp03.ex01;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        VerificarCuenta vc = new VerificarCuenta();
+        Thread Luis = new Thread(vc, "Luis");
+        Thread Manuel = new Thread(vc, "Manuel");
+        Luis.start();
+        Manuel.start();
+    }
+}
