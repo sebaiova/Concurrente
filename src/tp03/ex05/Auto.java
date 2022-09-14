0d462711a97e7e11e91698bc3d7aac8cb1c26363
Sebastian Iovaldi <sebastian.iovaldi@est.fi.uncoma.edu.ar>
2022-09-14 19:32:14 -0300

    eliminar atributo

--- src/tp03/ex05/Auto.java
+++ src/tp03/ex05/Auto.java
@@ -2,16 +2,9 @@ package tp03.ex05;
 
 public class Auto extends Vehiculo {
     
-    private int km;
 
     public Auto(int capacidadTanque, String patente, Surtidor surtidor) 
     {
         super(patente, capacidadTanque, surtidor);
-        this.km = 0;
-    }
-
-    public int getKm() 
-    {
-        return km;
     }
 }
