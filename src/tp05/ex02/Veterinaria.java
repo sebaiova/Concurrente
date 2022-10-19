d786dae0f1f5289c36890f171f0a34a45b9f6045
Sebastian Iovaldi <sebastian.iovaldi@est.fi.uncoma.edu.ar>
2022-10-19 19:06:30 -0300

    no termine nada

--- src/tp05/ex02/Veterinaria.java
+++ src/tp05/ex02/Veterinaria.java
@@ -4,26 +4,72 @@ import java.util.concurrent.Semaphore;
 
 public class Veterinaria {
     
-    private final Semaphore lugaresSemaphore;
-    private final Semaphore perrosSemaphore;
+    private final Semaphore perroSemaphore;
     private final Semaphore gatoSemaphore;
+    private final Semaphore mutex;
+    private final int capacidad;
+   // private final int  
 
-    public Veterinaria(int lugares)
+    public Veterinaria(int capacidad)
     {
-        lugaresSemaphore = new Semaphore(lugares);
-        perrosSemaphore = new Semaphore(1);
-        gatoSemaphore = new Semaphore(1);
+        perroSemaphore = new Semaphore(capacidad);
+        gatoSemaphore = new Semaphore(capacidad);
+        mutex = new Semaphore(1);
+        this.capacidad = capacidad;
     }
 
-    public void entraPerro() throws InterruptedException
+    public void entraPerro()
     {
-        perrosSemaphore.acquire();
-        lugaresSemaphore.acquire();
-        perrosSemaphore.release();
+        try { mutex.acquire();} catch (InterruptedException e1) { }
+        
+        
+
+
+        if(noHayPerros()) 
+            try { gatoSemaphore.acquire(capacidad);} catch (Exception e) {}
+        
+        try { perroSemaphore.acquire(1);} catch (Exception e) {}
+        System.out.println("Entra perro.");
+
+        mutex.release();
     }
 
-    public void salirPerro()
+    public void salePerro()
     {
-        lugaresSemaphore.release();
+        perroSemaphore.release(1);
+        if(noHayPerros())
+            gatoSemaphore.release(capacidad);
+        System.out.println("Sale perro.");
+    }
+
+    public void entraGato()
+    {
+        try { mutex.acquire();} catch (InterruptedException e1) { }
+
+        if(noHayGatos()) 
+            try { perroSemaphore.acquire(capacidad); } catch (InterruptedException e) { }
+        
+        try { gatoSemaphore.acquire(); } catch (InterruptedException e) { }
+        System.out.println("Entra gato.");
+
+        mutex.release();
+    }
+
+    public void saleGato()
+    {
+        gatoSemaphore.release();
+        if(noHayGatos())
+            perroSemaphore.release(capacidad);
+        System.out.println("Sale gato.");
+    }
+
+    public boolean noHayPerros()
+    {
+        return perroSemaphore.availablePermits() == capacidad;
+    }
+
+    public boolean noHayGatos()
+    {
+        return gatoSemaphore.availablePermits() == capacidad;
     }
 }
